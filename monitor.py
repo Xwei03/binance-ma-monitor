0fb385969e27e0dbc177d9447649a5e42a461375
Xwei03 <axdnh520@gmail.com>
2026-09-21 20:09:40 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -9,7 +9,7 @@ FEISHU_WEBHOOK = os.environ.get("FEISHU_WEBHOOK")
 # 监控的周期（OKX支持这些周期）
 INTERVALS = ["15m", "30m", "1H", "4H", "1D"]
 # 粘合阈值 (1% 即 0.01)
-THRESHOLD = 0.0025
+THRESHOLD = 0.002
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
